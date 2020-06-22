168ddd7f64f6ccd1a3965786334eb025528e054f
森本 義基 <yoshiki.morimoto@rakus.internal>
2020-06-22 17:34:59 +0900

    演習2のテスト仕様書作成

--- src/main/java/com/example/controller/HotelController.java
+++ src/main/java/com/example/controller/HotelController.java
@@ -2,11 +2,25 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.service.HotelService;
 
 @Controller
 @RequestMapping("/ex02")
 public class HotelController {
 	@Autowired
-	private
+	private HotelService service;
+	
+	@RequestMapping("")
+	public String index() {
+		
+		return "hotel-search";
+	}
+	
+	@RequestMapping("/search")
+	public String search() {
+		return "hotel-search";
+	}
 }
