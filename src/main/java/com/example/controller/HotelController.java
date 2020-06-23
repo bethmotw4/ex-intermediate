661cd3e36b1af64d38654969d3072df8dee0f3ed
森本 義基 <yoshiki.morimoto@rakus.internal>
2020-06-23 14:05:30 +0900

    演習2 のJavadocコメントを修正

--- src/main/java/com/example/controller/HotelController.java
+++ src/main/java/com/example/controller/HotelController.java
@@ -14,23 +14,46 @@ import com.example.domain.Hotel;
 import com.example.form.HotelForm;
 import com.example.service.HotelService;
 
+/**
+ * ホテル検索に関する制御を行うコントローラ.
+ * 
+ * @author yoshiki.morimoto
+ *
+ */
 @Controller
 @RequestMapping("/ex02")
 public class HotelController {
 	@Autowired
 	private HotelService service;
 	
+	/**
+	 * 検索に使用するフォームのインスタンス化.
+	 * 
+	 * @return フォーム
+	 */
 	@ModelAttribute
 	public HotelForm setUpHotelForm() {
 		return new HotelForm();
 	}
 	
+	/**
+	 * 検索画面を表示する.
+	 * 
+	 * @return 検索画面
+	 */
 	@RequestMapping("")
 	public String index() {
-		
 		return "hotel-search";
 	}
 	
+	/**
+	 * 検索結果を表示する.
+	 * 
+	 * @param form フォーム
+	 * @param result エラーメッセージ
+	 * @param model リクエストスコープ
+	 * @return 成功：検索結果画面, 失敗：検索画面
+	 */
 	@RequestMapping("/search")
 	public String search(@Validated HotelForm form, BindingResult result, Model model) {
 		if (result.hasErrors()) {
--- src/main/java/com/example/form/HotelForm.java
+++ src/main/java/com/example/form/HotelForm.java
@@ -2,11 +2,15 @@ package com.example.form;
 
 import javax.validation.constraints.Pattern;
 
+/**
+ * ホテル検索に使用するフォーム.
+ * 
+ * @author yoshiki.morimoto
+ *
+ */
 public class HotelForm {
-//	または空白を付けたい
+	/** 価格 */
 	@Pattern(regexp = "^[1-9][0-9]*$|\\s*", message = "金額の形式が不正です")
-//	@Pattern(regexp = "^[1-9][0-9]*$", message = "金額の形式が不正です")
-//	@Pattern(regexp = "\\s*", message = "金額の形式が不正です")
 	private String price;
 
 	public String getPrice() {
